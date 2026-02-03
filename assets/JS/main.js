62c68a9bf709fbaaec58158680f9876404b18fed
Francesca Golzio <francescagolzio@gmail.com>
2026-02-03 16:32:26 +0100

    add rule to n*5

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -6,22 +6,24 @@ console.log("🟢 I'm here")
 
 /* Program implementation */
 
-  /* Print numbers 1-100 */
-  for(let i = 1; i <= 100; i++) {
-    console.log(i)
-  
-    /* (check) IF n multiple of 3 */
-    if(i % 3 === 0) {
-      // Print 'Fizz'
-      console.log('Fizz')
-    }
-  }
-    
-  // (check) IF n multiple of 5
+/* Print numbers 1-100 */
+for (let i = 1; i <= 100; i++) {
+  console.log(i)
+
+  /* (check) IF n multiple of 3 */
+  if (i % 3 === 0) {
+    // Print 'Fizz'
+    console.log('Fizz')
+  } else if (i % 5 === 0) { /* (check) IF n multiple of 5 */
     // Print 'Buzz'
-    
-    // (check) IF n multiple of 3 AND 5
-      // Print 'FizzBuzz'
+    console.log('Buzz')
+  }
+}
+
+
+
+// (check) IF n multiple of 3 AND 5
+// Print 'FizzBuzz'
 
 
 
