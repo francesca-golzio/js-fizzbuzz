bc1a50152d8f154cbb801641acb7529fe99df443
Francesca Golzio <francescagolzio@gmail.com>
2026-02-03 16:19:08 +0100

    add computational thoughts

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,12 +1,22 @@
 /* Is this working? */
 console.log("🟢 I'm here")
 
+/* Collecting data */
 
 
+/* Program implementation */
 
+  // Print numbers 1-100
+  
 
-
-
+  // (check) IF n multiple of 3
+    // Print 'Fizz'
+    
+  // (check) IF n multiple of 5
+    // Print 'Buzz'
+    
+    // (check) IF n multiple of 3 AND 5
+      // Print 'FizzBuzz'
 
 
 
