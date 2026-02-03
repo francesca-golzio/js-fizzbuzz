e5af9b6d0b5de06026fbd56bafa5e09f0e1bfcbc
Francesca Golzio <francescagolzio@gmail.com>
2026-02-03 17:46:43 +0100

    make strings overwrite numbers

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,33 +1,33 @@
 /* Is this working? */
 console.log("🟢 I'm here")
 
-/* Collecting data */
-
-
 /* Program implementation */
 
-/* Print numbers 1-100 */
-for (let i = 1; i <= 100; i++) {
-  console.log(i)
+  /* Print numbers 1-100 */
+  for (let i = 1; i <= 100; i++) {
+    
+    /* (check) IF number (i) isn't multiple of 3 AND 5 */
+    if (!(i % 3 === 0) && !(i % 5 === 0)) {
+      // Print 'FizzBuzz'
+      console.log(i)
 
-  if ((i % 3 === 0) && (i % 5 === 0)) {
-    // ↑↑ (check) IF number (i) multiple of 3 AND 5
-    // Print 'FizzBuzz'
-    console.log('FizzBuzz')
-  } else if (i % 3 === 0) {
-    /* ↑↑ (check) IF number (i) multiple of 3 */
-    // Print 'Fizz'
-    console.log('Fizz')
-  } else if (i % 5 === 0) { 
-    /* ↑↑ (check) IF number (i) multiple of 5 */
-    // Print 'Buzz'
-    console.log('Buzz')
-  } 
+      /* (check) IF number (i) multiple of 3 AND 5 */
+    } else if ((i % 3 === 0) && (i % 5 === 0)) {
+        // Print 'FizzBuzz'
+        console.log('FizzBuzz')
 
-}
+        /* (check) IF number (i) multiple of 3 */
+      } else if (i % 3 === 0) {
+          // Print 'Fizz'
+          console.log('Fizz')
 
+          /* (check) IF number (i) multiple of 5 */
+        } else { 
+            if (i % 5 === 0) {
+              // Print 'Buzz'
+              console.log('Buzz')
+            }
+          }        
+  }
 
-
-
-
-
+  /* Note: The algorithm has been changed in order to have strings taking their own numbers places. */
